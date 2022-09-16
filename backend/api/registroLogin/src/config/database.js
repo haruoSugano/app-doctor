16ee26a19a1bea653167e2a9ba572a60ae63cd09
haruoSugano <haruosugano@icloud.com>
2022-09-16 03:10:57 -0300

    refatorado a config mongodb

--- backend/api/registroLogin/src/config/database.js
+++ backend/api/registroLogin/src/config/database.js
@@ -2,9 +2,16 @@
  * Arquivo: models/usuario.js
  * Descricao: arquivo responsavel pelas connectionStrings da aplicação: Mongodb
  */
+const mongoose = require("mongoose");
+const localDatabase = require("./localDatabase.json");
 
-module.exports = {
-    local: {
-        localUrl: "mongodb://localhost:27017/app-doctor-registro",
-    }
-}
+mongoose.connect(localDatabase.localURL, { useNewUrlParser: true }).then(() => {
+    console.log("A base de dados foi conectada com sucesso!");
+}, (err) => {
+    console.log(`Erro ao conectar com a base de dados: ${err}`);
+    process.exit();
+});
+
+mongoose.Promise = global.Promise;
+
+module.exports = mongoose;
