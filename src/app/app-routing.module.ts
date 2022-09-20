57b30c82cf5e9f15f243dbefe82cd08913a28897
Vinicius <Viniciussouzaalves@gmail.com>
2022-09-20 19:40:31 -0300

    Terceiro commit: adicionados os botões às telas de cadastro de pacientes e médicos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
 
+  /* { path: 'login', component: TelaLoginComponent },
+  { path: 'cadastro', component: TelaCadastroComponent }
+  , */
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
