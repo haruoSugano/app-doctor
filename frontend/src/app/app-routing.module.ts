2da0bf92af49207f4ca26b2edc33d6ee1c82a17e
Vinicius <Viniciussouzaalves@gmail.com>
2022-10-24 15:30:19 -0300

    Tela pesquisar médico e pesquisar paciente finalizadas

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -5,16 +5,19 @@ import { TelaLoginComponent } from './telas/tela-login/tela-login.component';
 import { TelaSistemaCadastroMedicoComponent } from './telas/tela-sistema-cadastro-medico/tela-sistema-cadastro-medico.component';
 import { TelaSistemaCadastroPacienteComponent } from './telas/tela-sistema-cadastro-paciente/tela-sistema-cadastro-paciente.component';
 import { TelaSistemaPesquisarMedicoComponent } from './telas/tela-sistema-pesquisar-medico/tela-sistema-pesquisar-medico.component';
+import { TelaSistemaPesquisarPacienteComponent } from './telas/tela-sistema-pesquisar-paciente/tela-sistema-pesquisar-paciente.component';
+
 import { TelaSistemaRealizarConsultaComponent } from './telas/tela-sistema-realizar-consulta/tela-sistema-realizar-consulta.component';
 import { TelaSistemaComponent } from './telas/tela-sistema/tela-sistema.component';
 
 const routes: Routes = [
   { path: '', component: TelaLoginComponent },
-  { path: 'home', component: TelaSistemaComponent, canActivate: [AuthGuard] },
-  { path: 'cadastrar-medico', component: TelaSistemaCadastroMedicoComponent, canActivate: [AuthGuard] },
-  { path: 'pesquisar-medico', component: TelaSistemaPesquisarMedicoComponent, canActivate: [AuthGuard] },
-  { path: 'cadastrar-paciente', component: TelaSistemaCadastroPacienteComponent, canActivate: [AuthGuard] },
-  { path: 'realizar-consulta', component: TelaSistemaRealizarConsultaComponent, canActivate: [AuthGuard] },
+  { path: 'home', component: TelaSistemaComponent},
+  { path: 'cadastrar-medico', component: TelaSistemaCadastroMedicoComponent, },
+  { path: 'pesquisar-medico', component: TelaSistemaPesquisarMedicoComponent, },
+  { path: 'cadastrar-paciente', component: TelaSistemaCadastroPacienteComponent, },
+  { path: 'pesquisar-paciente', component: TelaSistemaPesquisarPacienteComponent, },
+  { path: 'realizar-consulta', component: TelaSistemaRealizarConsultaComponent,  },
   { path: '', redirectTo: '/', pathMatch: 'full' }
 ];
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TelaLoginComponent } from './telas/tela-login/tela-login.component';
@@ -12,6 +11,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpRequestInterceptor } from "./shared/helpers/http.interceptor";
 import { TelaSistemaRealizarConsultaComponent } from './telas/tela-sistema-realizar-consulta/tela-sistema-realizar-consulta.component';
 import { TelaSistemaPesquisarMedicoComponent } from './telas/tela-sistema-pesquisar-medico/tela-sistema-pesquisar-medico.component';
+import { TelaSistemaPesquisarPacienteComponent } from './telas/tela-sistema-pesquisar-paciente/tela-sistema-pesquisar-paciente.component';
 
 @NgModule({
   declarations: [
@@ -21,8 +21,10 @@ import { TelaSistemaPesquisarMedicoComponent } from './telas/tela-sistema-pesqui
     TelaSistemaCadastroMedicoComponent,
     TelaSistemaCadastroPacienteComponent,
     TelaSistemaRealizarConsultaComponent,
-    TelaSistemaPesquisarMedicoComponent
+    TelaSistemaPesquisarMedicoComponent,
+    TelaSistemaPesquisarPacienteComponent
   ],
+
   imports: [
     BrowserModule,
     AppRoutingModule,
